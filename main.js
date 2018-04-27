8ee4b6056a731dc892b113b11088f2a7f9ad3e0c
sabindevkota <36958825+sabindevkota@users.noreply.github.com>
2018-04-27 23:25:56 +0930

    submission

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+var menu = document.querySelector('.ham-menu');
+
+menu.addEventListener("click", function(){
+  document.querySelector('.menu-mob').classList.toggle('menu-mob--open');
+
+});
+
+var show = document.querySelector('.clickLink');
+
+show.addEventListener("click", function(){
+
+})
